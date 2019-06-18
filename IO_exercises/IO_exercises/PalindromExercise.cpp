865dc0445cd2a80099e33e150b5e8a0e72639126
Henri Hirvelä <henri.hirvela@gmail.com>
2019-06-18 11:31:37 +0300

    Initialize project IO_exercises + First exercise Palindrom Exercise

---
+++ IO_exercises/IO_exercises/PalindromExercise.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Program takes a string input and tells the user if it's a palindrome.
+int main()
+{
+	string input;
+
+	cin >> input;
+
+	int wordLength = input.length() - 1;
+	int checkUntil = wordLength / 2;
+
+	for (int i = 0; i <= checkUntil; i++)
+	{
+
+		if (input[i] != input[wordLength - i])
+		{
+			cout << "NO";
+			break;
+		}
+		else if (i == checkUntil)
+		{
+			cout << "YES";
+		}
+
+	}
+}
