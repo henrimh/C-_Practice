57f05f5b6be0eea951e9bc9937718b96cb10ec02
Henri Hirvelä <henri.hirvela@gmail.com>
2019-07-03 14:58:50 +0300

    Commented

--- IO_exercises/SeatingArrangement/SeatingArrangement.cpp
+++ IO_exercises/SeatingArrangement/SeatingArrangement.cpp
@@ -3,6 +3,28 @@
 
 using namespace std;
 
+/*
+Description of the exercise 
+Akash and Vishal are quite fond of travelling. They mostly travel by railways. They were travelling in a train one day and they got interested in the seating arrangement of their compartment.
+
+So they got interested to know the seat number facing them and the seat type facing them. The seats are denoted as follows :
+
+Window Seat : WS
+Middle Seat : MS
+Aisle Seat : AS
+
+INPUT
+First line of input will consist of a single integer T denoting number of test-cases. Each test-case consists of a single integer N denoting the seat-number.
+
+OUTPUT
+For each test case, print the facing seat-number and the seat-type, separated by a single space in a new line.
+
+***FINNISH***
+Numerot 12 ja 6 pyörivät muutamassa kohtaa, koska tehtävän junassa loossit koostuvat 12 istumapaikasta. 
+Vastakkaisia numeroita ovat 1 12, 2 11, 3 10, 4 9, 5 8, 6 7.
+
+*/
+
 string SolveType(int x)
 {
 	x %= 6;
