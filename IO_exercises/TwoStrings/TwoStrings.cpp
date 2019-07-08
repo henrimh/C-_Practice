25674ee5ba610097e4c4769ef1bdf93195329991
Henri Hirvelä <henri.hirvela@gmail.com>
2019-07-08 16:33:52 +0300

    Added instruction to two strings. Should revise the exercise.

--- IO_exercises/TwoStrings/TwoStrings.cpp
+++ IO_exercises/TwoStrings/TwoStrings.cpp
@@ -1,3 +1,24 @@
+/*
+Given two strings of equal length, you have to tell whether they both strings are identical.
+
+Two strings S1 and S2 are said to be identical, if any of the permutation of string S1 is equal to the string S2. See Sample explanation for more details.
+
+Input :
+
+First line, contains an intger 'T' denoting no. of test cases.
+Each test consists of a single line, containing two space separated strings S1 and S2 of equal length.
+Output:
+
+For each test case, if any of the permutation of string S1 is equal to the string S2 print YES else print NO.
+Constraints:
+
+1<= T <=100
+1<= |S1| = |S2| <= 10^5
+String is made up of lower case letters only.
+
+Note : Use Hashing Concept Only . Try to do it in O(string length) .
+*/
+
 #include <iostream>
 #include <string>
 #include <algorithm>
