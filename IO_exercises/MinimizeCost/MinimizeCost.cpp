3d24953a31c9e5dac1177b5825e8d70a51fadc6b
Henri Hirvelä <henri.hirvela@gmail.com>
2019-07-22 15:06:18 +0300

    Minimize Cost DONE. Was reeeally hard to get it working with the speed needed. I needed to do major refactoring to my old code.

--- IO_exercises/MinimizeCost/MinimizeCost.cpp
+++ IO_exercises/MinimizeCost/MinimizeCost.cpp
@@ -1,4 +1,8 @@
 // Problem description: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/minimise-cost-89b54cb9/description/
+// tl;dr This program minimizes the absolute value (itseisarvo in finnish) of the array. The array has size N and only positive values may be moved to other indeces and only in range k.
+// ! You can split numbers !
+// For example array [3, -1, -2] with range k = 2. We can only move number 3. So we split it up and move 1 to array[1] and 2 to array[2]. So the Array becomes [0, 0, 0] thus making the absolute minimum value 0.
+// Another example is array [1, 2, 3, -3, -2, -1] with k = 2, it's minimum absolute value is 2. (As it becomes [1, 0, 0, 0, 0, -1])
 #include <iostream>
 #include <vector>
 
@@ -25,14 +29,15 @@ int main() {
 	{
 		// If array number is negative. It cannot be transferred.
 		if (arr[i] <= 0)
-		{
-			++j;
 			continue;
-		}
 
+		// If j has fallen out of range k. Move it back to the range.
+		if (last_j <= i - k)
+			last_j = i - k;
+
+		// Transfer positive numbers to negative numbers slots
 		for (j = last_j; j < arr.size() && j <= i + k; ++j)
 		{
-
 			if (arr[j] + transfer[j] < 0)
 			{
 				if (arr[i] >= abs(arr[j] + transfer[j]))
@@ -44,12 +49,6 @@ int main() {
 				{
 					transfer[j] += arr[i];
 					arr[i] = 0;
-
-					if (j + 1 <= i - k)
-						last_j = j;
-					else
-						last_j = (i - k <= 0 ? 0 : i - k);
-
 					break;
 				}
 			}
@@ -58,6 +57,7 @@ int main() {
 		}
 	}
 
+	//Count the absolute value of the array.
 	for (int i = 0; i < arr.size(); i++)
 	{
 		out += abs(arr[i] + transfer[i]);
